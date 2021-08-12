585550ab4a6dfa6c4034cb03e8bdaf7d444ab984
Romain Séguy <rom.seguy@gmail.com>
2021-08-12 15:48:39 +0200

    Fullscreen map

--- src/features/map/Map.tsx
+++ src/features/map/Map.tsx
@@ -1,7 +1,7 @@
 import type { LatLon } from "use-places-autocomplete";
 import type { IOrg } from "models/Org";
 import type { IEvent } from "models/Event";
-import React from "react";
+import React, { useRef } from "react";
 import { css } from "twin.macro";
 import GoogleMap from "google-map-react";
 import {
@@ -26,69 +26,6 @@ import { render } from "react-dom";
 const defaultCenter = {
   lat: 44.940694,
   lng: 1.989113
-};
-
-const FullscreenControl = (props: { onClick: (isFull: boolean) => void }) => {
-  const [isFull, setIsFull] = useState(false);
-  const onClick = () => {
-    setIsFull(!isFull);
-    props.onClick(!isFull);
-  };
-  return (
-    <Button
-      className="gm-control-active gm-fullscreen-control"
-      draggable="false"
-      aria-label="Passer en plein écran"
-      title="Passer en plein écran"
-      type="button"
-      onClick={onClick}
-      css={css`
-        background: none rgb(255, 255, 255);
-        border: 0px;
-        margin: 10px;
-        padding: 0px;
-        text-transform: none;
-        appearance: none;
-        position: absolute;
-        cursor: pointer;
-        user-select: none;
-        border-radius: 2px;
-        height: 40px;
-        width: 40px;
-        box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
-        overflow: hidden;
-        top: 0px;
-        right: 0px;
-      `}
-    >
-      <>
-        <img
-          src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23666%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
-          alt=""
-          css={css`
-            height: 18px;
-            width: 18px;
-          `}
-        />
-        <img
-          src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23333%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
-          alt=""
-          css={css`
-            height: 18px;
-            width: 18px;
-          `}
-        />
-        <img
-          src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23111%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
-          alt=""
-          css={css`
-            height: 18px;
-            width: 18px;
-          `}
-        />
-      </>
-    </Button>
-  );
 };
 
 const Marker = ({
@@ -171,150 +108,233 @@ const Marker = ({
   );
 };
 
-export const Map = React.memo(
-  ({
-    center,
-    items,
-    onFullscreenControlClick
-  }: {
-    center?: LatLon;
-    items: IEvent[] | IOrg[];
-    onFullscreenControlClick: (isFull: boolean) => void;
-  }) => {
-    const [mapsSdkState, setMapsSdkState] = useState({
-      loaded: false,
-      loading: false
+export const Map = function Map({
+  center,
+  items,
+  height,
+  onFullscreenControlClick,
+  ...props
+}: {
+  center?: LatLon;
+  items: IEvent[] | IOrg[];
+  height?: number;
+  onGoogleApiLoaded: () => void;
+  onFullscreenControlClick?: (isFull: boolean) => void;
+}) {
+  const FullscreenControl = () => {
+    const [isFull, setIsFull] = useState(false);
+    const onClick = () => {
+      const f = !isFull;
+      setIsFull(f);
+      onFullscreenControlClick && onFullscreenControlClick(f);
+    };
+
+    return (
+      <Button
+        className="gm-control-active gm-fullscreen-control"
+        draggable="false"
+        aria-label="Passer en plein écran"
+        title="Passer en plein écran"
+        type="button"
+        onClick={onClick}
+        css={css`
+          background: none rgb(255, 255, 255);
+          border: 0px;
+          margin: 10px;
+          padding: 0px;
+          text-transform: none;
+          appearance: none;
+          position: absolute;
+          cursor: pointer;
+          user-select: none;
+          border-radius: 2px;
+          height: 40px;
+          width: 40px;
+          box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
+          overflow: hidden;
+          top: 0px;
+          right: 0px;
+        `}
+      >
+        <>
+          <img
+            src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23666%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
+            alt=""
+            css={css`
+              height: 18px;
+              width: 18px;
+            `}
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23333%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
+            alt=""
+            css={css`
+              height: 18px;
+              width: 18px;
+            `}
+          />
+          <img
+            src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2018%2018%22%3E%3Cpath%20fill%3D%22%23111%22%20d%3D%22M0%200v6h2V2h4V0H0zm16%200h-4v2h4v4h2V0h-2zm0%2016h-4v2h6v-6h-2v4zM2%2012H0v6h6v-2H2v-4z%22/%3E%3C/svg%3E"
+            alt=""
+            css={css`
+              height: 18px;
+              width: 18px;
+            `}
+          />
+        </>
+      </Button>
+    );
+  };
+  const mapRef = useRef(null);
+  const options = {
+    fullscreenControl: false
+    // zoomControl: boolean,
+    // mapTypeControl: boolean,
+    // scaleControl: boolean,
+    // streetViewControl: boolean,
+    // rotateControl: boolean,
+  };
+
+  const [mapsSdkState, setMapsSdkState] = useState({
+    loaded: false,
+    loading: false
+  });
+
+  useEffect(() => {
+    const xhr = async () => {
+      try {
+        setMapsSdkState({ loaded: false, loading: true });
+        await loadMapsSdk();
+        setMapsSdkState({ loaded: true, loading: false });
+      } catch (error) {
+        console.log(error);
+        setMapsSdkState({ loaded: false, loading: false });
+      }
+    };
+
+    xhr();
+  }, []);
+
+  const hash: { [key: string]: boolean } = {};
+
+  const [markers, setMarkers] = useState(
+    items.map((item, index) => {
+      const key = `marker-${index}`;
+      let lat = "eventName" in item ? item.eventLat : item.orgLat;
+      let lng = "eventName" in item ? item.eventLng : item.orgLng;
+
+      if (lat && lng) {
+        const latLng = `${lat}_${lng}`;
+
+        if (hash[latLng]) {
+          lat = lat + (Math.random() - 0.5) / 1500;
+          lng = lng + (Math.random() - 0.5) / 1500;
+        } else hash[latLng] = true;
+      }
+
+      return {
+        key,
+        lat,
+        lng,
+        item
+      };
+    })
+  );
+
+  const onGoogleApiLoaded = ({ map, maps: api }: { map: any; maps: any }) => {
+    if (!map || !api) return;
+
+    props.onGoogleApiLoaded();
+
+    // if (mapRef.current) {
+    //   console.log(mapRef.current);
+    // }
+
+    if (!options.fullscreenControl) {
+      const controlButtonDiv = document.createElement("div");
+      render(<FullscreenControl />, controlButtonDiv);
+      map.controls[api.ControlPosition.TOP_RIGHT].push(controlButtonDiv);
+    }
+
+    const gMarkers = markers.map(
+      ({ lat, lng }) =>
+        new api.Marker({
+          position: { lat, lng }
+        })
+    );
+
+    gMarkers.map((marker) => {
+      return marker.setVisible(false);
     });
 
-    useEffect(() => {
-      const xhr = async () => {
-        try {
-          setMapsSdkState({ loaded: false, loading: true });
-          await loadMapsSdk();
-          setMapsSdkState({ loaded: true, loading: false });
-        } catch (error) {
-          console.log(error);
-          setMapsSdkState({ loaded: false, loading: false });
-        }
-      };
+    const cluster = new MarkerClusterer(map, gMarkers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      minimumClusterSize: 2
+    });
 
-      xhr();
-    }, []);
+    api.event.addListener(cluster, "clusteringend", () => {
+      const markersGrouped = cluster
+        .getClusters()
+        .map((cl) => cl.getMarkers())
+        .filter((marker) => marker.length > 1);
 
-    const hash: { [key: string]: boolean } = {};
+      const positions = markersGrouped.map((markers) =>
+        markers.map((marker) => ({
+          lat: marker.getPosition().lat(),
+          lng: marker.getPosition().lng()
+        }))
+      )[0];
 
-    const [markers, setMarkers] = useState(
-      items.map((item, index) => {
-        const key = `marker-${index}`;
-        let lat = "eventName" in item ? item.eventLat : item.orgLat;
-        let lng = "eventName" in item ? item.eventLng : item.orgLng;
-
-        if (lat && lng) {
-          const latLng = `${lat}_${lng}`;
-
-          if (hash[latLng]) {
-            lat = lat + (Math.random() - 0.5) / 1500;
-            lng = lng + (Math.random() - 0.5) / 1500;
-          } else hash[latLng] = true;
-        }
-
-        return {
-          key,
-          lat,
-          lng,
-          item
-        };
-      })
-    );
-
-    if (!mapsSdkState.loading && !mapsSdkState.loaded)
-      return (
-        <Alert status="error">
-          <AlertIcon />
-          Nous n'avons pas pu charger la carte. Êtes-vous connecté à internet ?
-        </Alert>
+      setMarkers(
+        markers.filter(({ lat, lng }) => {
+          let allow = true;
+          if (
+            positions &&
+            positions.find(
+              (position: { lat: number; lng: number }) =>
+                position.lat === lat && position.lng === lng
+            )
+          )
+            allow = false;
+          return allow;
+        })
       );
+    });
+  };
 
-    return mapsSdkState.loading ? (
-      <Spinner />
-    ) : (
-      <div style={{ height: "400px", width: "100%" }}>
-        <GoogleMap
-          defaultCenter={defaultCenter}
-          defaultZoom={10}
-          center={center}
-          options={(maps) => ({
-            fullscreenControl: false
-            // zoomControl: boolean,
-            // mapTypeControl: boolean,
-            // scaleControl: boolean,
-            // streetViewControl: boolean,
-            // rotateControl: boolean,
-          })}
-          yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({ map, maps: api }) => {
-            if (!map || !api) return;
-
-            const controlButtonDiv = document.createElement("div");
-            render(
-              <FullscreenControl onClick={onFullscreenControlClick} />,
-              controlButtonDiv
-            );
-
-            map.controls[api.ControlPosition.TOP_RIGHT].push(controlButtonDiv);
-
-            const gMarkers = markers.map(
-              ({ lat, lng }) =>
-                new api.Marker({
-                  position: { lat, lng }
-                })
-            );
-
-            gMarkers.map((marker) => {
-              return marker.setVisible(false);
-            });
-
-            const cluster = new MarkerClusterer(map, gMarkers, {
-              imagePath:
-                "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-              minimumClusterSize: 2
-            });
-
-            api.event.addListener(cluster, "clusteringend", () => {
-              const markersGrouped = cluster
-                .getClusters()
-                .map((cl) => cl.getMarkers())
-                .filter((marker) => marker.length > 1);
-
-              const positions = markersGrouped.map((markers) =>
-                markers.map((marker) => ({
-                  lat: marker.getPosition().lat(),
-                  lng: marker.getPosition().lng()
-                }))
-              )[0];
-
-              setMarkers(
-                markers.filter(({ lat, lng }) => {
-                  let allow = true;
-                  if (
-                    positions &&
-                    positions.find(
-                      (position: { lat: number; lng: number }) =>
-                        position.lat === lat && position.lng === lng
-                    )
-                  )
-                    allow = false;
-                  return allow;
-                })
-              );
-            });
-          }}
-        >
-          {markers.map((marker) => (
-            <Marker {...marker} />
-          ))}
-        </GoogleMap>
-      </div>
+  if (!mapsSdkState.loading && !mapsSdkState.loaded)
+    return (
+      <Alert status="error">
+        <AlertIcon />
+        Nous n'avons pas pu charger la carte. Êtes-vous connecté à internet ?
+      </Alert>
     );
-  }
-);
+
+  return mapsSdkState.loading ? (
+    <Spinner />
+  ) : (
+    <div style={{ height: height + "px" || "400px", width: "100%" }}>
+      <GoogleMap
+        defaultCenter={defaultCenter}
+        defaultZoom={10}
+        center={center}
+        ref={mapRef}
+        // style={{
+        //   position: "absolute",
+        //   left: 0,
+        //   right: 0,
+        //   top: 0,
+        //   bottom: 0
+        // }}
+        options={(maps) => options}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={onGoogleApiLoaded}
+      >
+        {markers.map((marker) => (
+          <Marker {...marker} />
+        ))}
+      </GoogleMap>
+    </div>
+  );
+};
--- src/features/modals/MapModal.tsx
+++ src/features/modals/MapModal.tsx
@@ -1,7 +1,7 @@
 import type { LatLon } from "use-places-autocomplete";
 import type { IOrg } from "models/Org";
 import type { IEvent } from "models/Event";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import {
   Modal,
   ModalOverlay,
@@ -9,10 +9,24 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
-  useDisclosure
+  useDisclosure,
+  Box,
+  Text
 } from "@chakra-ui/react";
 import { Map } from "features/map/Map";
 import { MapSearch } from "features/map/MapSearch";
+import { css } from "twin.macro";
+
+type SizeMap = {
+  defaultSize: {
+    enabled: boolean;
+    height?: number;
+  };
+  fullSize: {
+    enabled: boolean;
+    height?: number;
+  };
+};
 
 export const MapModal = ({
   items,
@@ -21,9 +35,16 @@ export const MapModal = ({
   onClose: () => void;
   items?: IEvent[] | IOrg[];
 }) => {
+  const divRef = useRef<HTMLDivElement>(null);
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
   const [center, setCenter] = useState<LatLon>();
-  const [size, setSize] = useState<string | undefined>();
+
+  const [size, setSize] = useState<SizeMap>({
+    defaultSize: { enabled: true },
+    fullSize: { enabled: false }
+  });
+
+  const [height, setHeight] = useState<number>();
 
   return (
     <Modal
@@ -32,37 +53,84 @@ export const MapModal = ({
         props.onClose && props.onClose();
         onClose();
       }}
-      size={size}
+      size={size.fullSize.enabled ? "full" : undefined}
       closeOnOverlayClick
     >
       <ModalOverlay>
-        <ModalContent mt={size === "full" ? 0 : undefined}>
-          <ModalHeader>
-            {items &&
-              items[0] &&
-              `Carte des ${
-                "eventName" in items[0] ? "événements" : "organisations"
-              }`}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
+        <ModalContent my={0} minHeight="100vh">
+          {size.defaultSize.enabled && (
             <>
-              {Array.isArray(items) && items.length > 0 ? (
-                <>
-                  <MapSearch setCenter={setCenter} />
-                  <Map
-                    center={center}
-                    items={items}
-                    onFullscreenControlClick={(isFull: boolean) => {
-                      setSize(isFull ? "full" : undefined);
-                    }}
-                    {...props}
-                  />
-                </>
-              ) : (
-                "Il n'y a encore rien à afficher sur cette carte, revenez plus tard !"
-              )}
+              <ModalHeader>
+                {items &&
+                  items[0] &&
+                  `Carte des ${
+                    "eventName" in items[0] ? "événements" : "organisations"
+                  }`}
+              </ModalHeader>
+              <ModalCloseButton />
             </>
+          )}
+          <ModalBody ref={divRef} p={size.fullSize.enabled ? 0 : undefined}>
+            {Array.isArray(items) && items.length > 0 ? (
+              <>
+                {size.defaultSize.enabled && (
+                  <MapSearch setCenter={setCenter} />
+                )}
+                <Map
+                  center={center}
+                  height={height}
+                  items={items}
+                  onGoogleApiLoaded={() => {
+                    if (divRef && divRef.current) {
+                      const height = divRef.current.offsetHeight - 80;
+                      const newSize: SizeMap = {
+                        ...size,
+                        defaultSize: { enabled: true, height }
+                      };
+                      setSize(newSize);
+                      setHeight(height);
+                    }
+                  }}
+                  onFullscreenControlClick={(isFull: boolean) => {
+                    if (isFull) {
+                      const newSize: SizeMap = {
+                        fullSize: { enabled: true },
+                        defaultSize: { enabled: false }
+                      };
+                      setSize(newSize);
+                      setTimeout(() => {
+                        if (divRef && divRef.current) {
+                          const height = divRef.current.offsetHeight;
+                          newSize.fullSize.height = height;
+                          setSize(newSize);
+                          setHeight(height);
+                        }
+                      }, 200);
+                    } else {
+                      const newSize: SizeMap = {
+                        defaultSize: { enabled: true },
+                        fullSize: { enabled: false }
+                      };
+                      setSize(newSize);
+                      setTimeout(() => {
+                        if (divRef && divRef.current) {
+                          const height = divRef.current.offsetHeight - 210;
+                          newSize.fullSize.height = height;
+                          setSize(newSize);
+                          setHeight(height);
+                        }
+                      }, 200);
+                    }
+                  }}
+                  {...props}
+                />
+              </>
+            ) : (
+              <Text>
+                Il n'y a encore rien à afficher sur cette carte, revenez plus
+                tard !
+              </Text>
+            )}
           </ModalBody>
         </ModalContent>
       </ModalOverlay>
