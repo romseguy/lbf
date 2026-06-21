4359b0bd86e7a27461dc73bfbdf72363a34336de
romseguy <contact@romseguy.com>
2026-06-21 10:33:26 +0200

    y

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,17 +47,14 @@ const App = wrapper.withRedux(
               strategy="lazyOnload"
               dangerouslySetInnerHTML={{
                 __html: `
-                  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                   (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110024277', 'ym');
 
-                  ym(98157053, "init", {
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true
-                  });
+    ym(110024277, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
                 `
               }}
             />
